c4be400835313f8480b17ddef4e587b0249d836d
Mathilde Hetru <76809829+mathhetru@users.noreply.github.com>
2023-05-09 11:16:45 +0200

    starting ingredientsMatchingWithSearchWord

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -7,34 +7,49 @@ export function searchBar(recipes) {
     //Genère les recettes par défaut
     generateRecipes(recipes);
 
-    //ARRAYS
-    let newSearchOfRecipes = [];
-
     searchBarInput.addEventListener("input", function () {
         const searchWord = searchBarInput.value.toLowerCase().trim();
         const lengthSearchWord = searchWord.length;
 
         if (lengthSearchWord > 2) {
-            // remise à zéro du tableau de recettes cherchées
-            newSearchOfRecipes = [];
+            //ARRAYS
+            let newSearchOfRecipes = [];
+            let newSearchPerName = [];
+            let newSearchPerIngredients = [];
 
-            // ! changer par un filter() et faire la recherche sur les ingrédients et la description
-            // pour chaque recette, reprendre le nom, si le mot recherché est inclus dans un des mots du nom alors ajouter cette recette dans le tableau 
+            // filtrer chaque recette par le mot recherché et l'ajouter dans le tableau
+            newSearchPerName = recipes.filter(oneRecipe => oneRecipe.name.toLowerCase().includes(searchWord));
+            
+            // const ingredientsMatchingWithSearchWord = recipes.filter(oneRecipe => oneRecipe.ingredients.filter(oneIngredient => oneIngredient.ingredient.toLowerCase().includes(searchWord)));
+            
+            // pour chaque recette, filtrer les ingredients par le mot recherché et l'ajouter au tableau
             recipes.forEach(oneRecipe => {
-                const nameOfOneRecipe = oneRecipe.name;
+                const ingredientsOfOneRecipe = oneRecipe.ingredients;
+                const ingredientsMatchingWithSearchWord = ingredientsOfOneRecipe.filter(oneIngredient => oneIngredient.ingredient.toLowerCase().includes(searchWord));
 
-                if (nameOfOneRecipe.toLowerCase().includes(searchWord)) {
-                    newSearchOfRecipes.push(oneRecipe);
-                }
+                if (ingredientsMatchingWithSearchWord != ""){
+                    newSearchPerIngredients.push(oneRecipe);
+                    // console.table(newSearchPerIngredients);
+                }             
+            });    
 
-                //* OLD CODE
+            //** OLD CODE
+            // recipes.forEach(oneRecipe => {
+            //     const nameOfOneRecipe = oneRecipe.name;
+
+            //*V2
+            //     if (nameOfOneRecipe.toLowerCase().includes(searchWord)) {
+            //         newSearchOfRecipes.push(oneRecipe);
+            //     }
+
+                //*V1
                 // let eachWordOfOneRecipe = nameOfOneRecipe.split(" ");
                 // eachWordOfOneRecipe.forEach(oneWord => {
                 //     if (oneWord.toLowerCase().startsWith(searchWord)){
                 //         newSearchOfRecipes.push(oneRecipe);
                 //     }
                 // });
-            });
+            // });
             generateRecipes(newSearchOfRecipes);
         } else {
             generateRecipes(recipes);
