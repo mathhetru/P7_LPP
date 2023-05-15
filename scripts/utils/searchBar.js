dce09e482eae6db697b91f4ff9e3d97d308dcb1f
Mathilde Hetru <76809829+mathhetru@users.noreply.github.com>
2023-05-15 12:23:06 +0200

    add set

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -5,8 +5,14 @@ export function searchBar(recipes) {
     //DOM
     const searchBarInput = document.querySelector(".search-bar__input");
 
-    //Genère les recettes par défaut
+    // ARRAYS of all items
+    let allIngredients = recipes.map(newSearchOfRecipes => newSearchOfRecipes.ingredients).flat();
+    let allAppliance = [];
+    let allUstensils = [];
+
+    // Genère les recettes par défaut
     generateRecipes(recipes);
+    generateDPIngredientsDOM(allIngredients);
 
     searchBarInput.addEventListener("input", function () {
         const searchWord = searchBarInput.value.toLowerCase().trim();
@@ -14,13 +20,20 @@ export function searchBar(recipes) {
         const oupsSentence = document.querySelector(".main-oups");
 
         if (lengthSearchWord > 2) {
-            // ARRAYS
-            let arrayOfIngredients = [];
-            let arrayOfAppliance = [];
-            let arrayOfUstensils = [];
 
-            // filtrer chaque recette par le mot recherché et l'ajouter au tableau
+            // filter each recipe with the search word and add in the array
             const newSearchOfRecipes = recipes.filter(oneRecipe => filterPerSearchWord(oneRecipe, searchWord));
+            // console.log(newSearchOfRecipes);
+
+            // ARRAYS of items searched
+            let allIngredientsOfRecipesSearched = newSearchOfRecipes.flatMap(newSearchOfRecipes => newSearchOfRecipes.ingredients);
+            let ingredientsOfRecipesSearched = new Set(allIngredientsOfRecipesSearched);
+
+            console.log(allIngredientsOfRecipesSearched);
+            console.log(ingredientsOfRecipesSearched);
+
+            let applianceOfRecipesSearched = [];
+            let ustensilsOfRecipesSearched = [];
 
             if (newSearchOfRecipes.length === 0) {
                 oupsSentence.classList.remove("hide");
@@ -29,8 +42,10 @@ export function searchBar(recipes) {
             }
             
             generateRecipes(newSearchOfRecipes);
+            generateDPIngredientsDOM(ingredientsOfRecipesSearched);
         } else {
             generateRecipes(recipes);
+            generateDPIngredientsDOM(allIngredients);
         };
     });
 }
